fb23dc767511c7d92fa438027cd4168eeaa91750
MridulModi12 <modi.mridul0828@gmail.com>
2024-09-28 23:10:40 +0530

    Modified index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,35 @@
-const express = require(express)
+const express = require("express")
+const { createTodo, updateTodo } = require("./types.js")
 const app = express()
 
 app.use(express.json())
 
-app.post("/addTodo") 
+app.post("/todo", (req,res)=> {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+    // put it in mongodb
+}) 
 
-app.get("/getTodo")
+app.get("/todos", (req,res)=> {
 
-app.put("updateTodo")
+})
+
+app.put("/completed",(req,res)=> {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
+})
 
 
 app.listen(3000)
