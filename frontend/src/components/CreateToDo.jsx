40b2427a8a38a6d1936add1c92331e661b9d1cfe
MridulModi12 <modi.mridul0828@gmail.com>
2024-09-29 23:27:26 +0530

    Completed frontend post request

--- frontend/src/components/CreateToDo.jsx
+++ frontend/src/components/CreateToDo.jsx
@@ -1,16 +1,42 @@
 export function CreateToDo() {
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
     return <div>
+
         <input style={{
             margin: 5,
             padding: 5
-        }}type="text" placeholder="title"></input> <br/>
+        }}type="text" placeholder="title" onChange={function(e) {
+            setTitle(e.target.value)
+        }}></input> <br/>
+
         <input style={{
             margin: 5,
             padding: 5
-        }}type="text" placeholder="description"></input> <br/>
+        }}type="text" placeholder="description" onChange={function(e) {
+            setDescription(e.target.value)
+        }}></input> <br/>
+
         <button style={{
             margin: 5,
             padding: 5
+        }} onClick={() => {
+            fetch("localhost:3000/todo" , {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }), 
+                headers: {
+                    "Content-type" : "application/json"
+                }
+            }) 
+                .then(async function(res) {
+                    const json = await res.json()
+                    alert("Todo added")
+                })
         }}>Add a todo</button>
     </div>
 }
