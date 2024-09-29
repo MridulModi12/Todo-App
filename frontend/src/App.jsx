6db1bbea55d5d0577d770c3450a3e6478a464bba
MridulModi12 <modi.mridul0828@gmail.com>
2024-09-29 19:45:02 +0530

    Created frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CreateToDo } from './components/CreateToDo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <CreateToDo></CreateToDo>
+      </div>
+    </>
+  )
+}
+
+export default App
